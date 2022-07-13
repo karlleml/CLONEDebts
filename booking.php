eedeb306448f02af8865af7653cdc2e065784e17
Karlleml1 <qkladeleon@tip.edu.ph>
2022-07-13 14:17:11 +0800

    init

---
+++ booking.php
@@ -0,0 +1,13 @@
+<?php
+include("config.php");
+include("dbh.php");
+?>
+<html>
+<head>
+    <title>Admin Trail</title>
+    <link rel="icon" href="image/TRANSPARENT BG.png">
+    <link rel="stylesheet" href="fontawesome-free-5.15.1-web/css/all.css" type="text/css" />
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+    <link rel='stylesheet' href='https://use.fontawesome.com/releases/v5.3.1/css/all.css'>
+</head>
+    </html>
